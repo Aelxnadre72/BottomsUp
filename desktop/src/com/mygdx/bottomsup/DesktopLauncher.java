4523a696748460665620d1f24357609efedefa87
Martine Erdal Mansåker <martiman@stud.ntnu.no>
2023-03-10 13:37:25 +0100

    Updated configuration in DesktopLauncher

--- desktop/src/com/mygdx/bottomsup/DesktopLauncher.java
+++ desktop/src/com/mygdx/bottomsup/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.mygdx.bottomsup;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.bottomsup.Game;
+import com.mygdx.bottomsup.BottomsUp;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("bottoms-up");
-		new Lwjgl3Application(new Game(), config);
+		new Lwjgl3Application(new BottomsUp(), config);
 	}
 }
