8221a781b31f913f1103df3048c1297e8c49c90a
Alexander Le <alexle@stud.ntnu.no>
2023-03-13 13:56:48 +0100

    #13 Initial firebase setup

--- ios/src/com/mygdx/bottomsup/IOSLauncher.java
+++ ios/src/com/mygdx/bottomsup/IOSLauncher.java
@@ -5,13 +5,13 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.mygdx.bottomsup.Game;
+import com.mygdx.bottomsup.BottomsUp;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new Game(), config);
+        return new IOSApplication(new BottomsUp(), config);
     }
 
     public static void main(String[] argv) {
