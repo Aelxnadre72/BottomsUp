557bd093f5d83c3db191c83588ae1e5184bbcfbc
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2023-03-14 12:44:48 +0100

    Start implementation

---
+++ core/src/sprites/Player.java
@@ -0,0 +1,13 @@
+package sprites;
+
+public class Player {
+    private int playerNum;
+
+    public Player(int playerNum) {
+        this.playerNum = playerNum;
+    }
+
+    public int getPlayerNum() {
+        return playerNum;
+    }
+}
