676f8075c3191c72aa7bafa95e0293fcfe015d36
Hang Celin Le <hcle@stud.ntnu.no>
2023-03-21 16:14:28 +0100

    (#3)created input field

--- core/src/com/mygdx/bottomsup/BottomsUp.java
+++ core/src/com/mygdx/bottomsup/BottomsUp.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import screens.GameScreenManager;
+import screens.JoinGameScreen;
 import screens.MainMenuScreen;
 
 
@@ -20,7 +21,7 @@ public class BottomsUp extends Game {
 		batch = new SpriteBatch();
 		gsm = new GameScreenManager();
 		ScreenUtils.clear(0, 0, 0, 1);
-		gsm.push(new MainMenuScreen(gsm));
+		gsm.push(new JoinGameScreen(gsm));
 	}
 
 	@Override
--- core/src/screens/JoinGameScreen.java
+++ core/src/screens/JoinGameScreen.java
@@ -1,3 +1,6 @@
+package screens;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,21 +12,22 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
+import org.w3c.dom.Text;
+
+import screens.GameScreenManager;
+import screens.TextInputListener;
+
 public class JoinGameScreen extends Screen {
 
     private Texture enterButton;
     private  Texture background;
     private Texture logo;
-
-    private Texture textfield;
-
     private final TextField  pin_input;
     private TextInputListener listener;
 
     private TextField.TextFieldListener txtListener;
     private Skin uiskin;
     private Stage stage;
-    private Table table;
 
     public JoinGameScreen(GameScreenManager gsm) {
         super(gsm);
@@ -35,60 +39,54 @@ public class JoinGameScreen extends Screen {
 
         stage = new Stage(new ScreenViewport());
 
-        table = new Table();
-        table.setWidth(stage.getWidth());
-        table.align(Align.center | Align.top);
-        table.setPosition(0, Gdx.graphics.getHeight());
-        stage.addActor(table);
-
         pin_input= new TextField("", uiskin);
         pin_input.setSize(300, 50);
         pin_input.setPosition((300), 500);
 
         listener = new TextInputListener();
         pin_input.setTextFieldListener(txtListener);
-        table.add(pin_input).width(400).fill();
         stage.addActor(pin_input);
-    }
 
-    public void keyTyped(TextField textField, char key) {
-        if (key == '\n')
-        {
-            textField.getOnscreenKeyboard().show(false);
-        }
+        Gdx.input.getTextInput(listener, "", "", "Enter game pin");
+        Gdx.input.setInputProcessor(stage);
+        Gdx.input.setOnscreenKeyboardVisible(false);
     }
 
     @Override
     public void handleInput(){
-
-        keyTyped(pin_input, 'h');
+        if (Gdx.input.justTouched()){
+            Gdx.input.setOnscreenKeyboardVisible(true);
+        }
     }
 
     @Override
-    handleInput();
-}
+    public void update() {
+        handleInput();
+    }
 
 
     @Override
     public void render(SpriteBatch sb) {
-        float sizeButton = (float) ((float)Gdx.graphics.getWidth() * 0.6);
-        float sizeLogo = (float) ((float)Gdx.graphics.getWidth() * 0.6);
-        float sizeLogo2 = (float) (sizeLogo * 0.75);
 
-        Gdx.input.getTextInput(listener, "", "Enter game pin", "Hint Value");
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+
+        float scaleWidth = (float)(Gdx.graphics.getWidth() * 0.6);
+        float scaleButton = (float)(scaleWidth * 0.3);
+        float scaleLogo = (float)(scaleWidth * 0.75);
         float delta = Gdx.graphics.getDeltaTime();
-        stage.act(delta);
-        stage.draw();
-
-        Gdx.input.setInputProcessor(stage);
-        Gdx.input.setOnscreenKeyboardVisible(true);
 
         sb.begin();
-        sb.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        @Override
-        public void dispose(){
-
-            stage.dispose();
-        }
+        sb.draw(background, 0, 0, width, height);
+        //sb.draw(logo, (float)(width * 0.2), (float)(height * 0.6), scaleWidth, scaleLogo);
+        sb.draw(enterButton, (float)(width * 0.2), (float)(height * 0.25), scaleWidth, scaleButton);
+        stage.act(delta);
+        stage.draw();
+        sb.end();
     }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
