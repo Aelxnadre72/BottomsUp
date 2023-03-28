9e5dd9e006dbf14ac80fae8b4a8f2718a1c3c790
einartmy <einartmy@stud.ntnu.no>
2023-03-28 15:47:38 +0200

    Making the sizes standard and more dynamic

--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -66,47 +66,52 @@ public class GameScreen extends Screen {
         float width = Gdx.graphics.getWidth();
         float height = Gdx.graphics.getHeight();
 
-        float scaleWidthMain = (float)(Gdx.graphics.getWidth() * 0.6);
-        float scaleWidthPlayers = (float)(Gdx.graphics.getWidth() * 0.32);
-        float scaleWidthBtn = (float)(Gdx.graphics.getWidth() * 0.47);
-        float scaleButton = (float)(scaleWidthBtn * 0.65);
+        // standard sizes for player screens
+        float widthMain = (float)(width * 0.6);
+        float heightMain = (float)(height * 0.55);
+        float widthPlayers = (float)(width * 0.32);
+        float heightPlayers = (float) (height * 0.21);
 
+        // standard sizes for buttons
+        float widthBtn = (float)(width * 0.47);
+        float heightBtn = (float)(widthBtn * 0.65);
+
+        // Scales for width and height for the different buttons and screen
         double heightScaleTop = 0.141;
         double heightScaleBot = 0.01;
-        double widthScaleLeft = 0.02;
-        double widthScaleRight = 2*widthScaleLeft + 0.47;
-        double widthScalePlayers = 0.6;
+        double widthScale = 0.02;
+        double heightScalePlayers = heightScaleBot + heightScaleTop;
 
         sb.begin();
         sb.draw(background, 0, 0, width, height);
         //sb.draw(logo, (float)(width * 0.2), (float)(height * 0.6), scaleWidth, scaleLogo);
-        sb.draw(btn1, (float)(width * widthScaleLeft), (float)(height * heightScaleTop), scaleWidthBtn, scaleButton);
-        sb.draw(btn2, (float)(width * widthScaleRight), (float)(height * heightScaleTop), scaleWidthBtn, scaleButton);
-        sb.draw(btn3, (float)(width * widthScaleLeft), (float)(height * heightScaleBot), scaleWidthBtn, scaleButton);
-        sb.draw(btn4, (float)(width * widthScaleRight), (float)(height * heightScaleBot), scaleWidthBtn, scaleButton);
-        //sb.draw(joinButton, (float)(width * 0.2), (float)(height * 0.25), scaleWidth, scaleButton);
+        sb.draw(btn1, (float)(width * widthScale),              (float)(height * heightScaleTop), widthBtn, heightBtn);
+        sb.draw(btn2, (float)(width * 2*widthScale + widthBtn), (float)(height * heightScaleTop), widthBtn, heightBtn);
+        sb.draw(btn3, (float)(width * widthScale),              (float)(height * heightScaleBot), widthBtn, heightBtn);
+        sb.draw(btn4, (float)(width * 2*widthScale + widthBtn), (float)(height * heightScaleBot), widthBtn, heightBtn);
 
-        double heightScalePlayers = 0.22;
         sb.draw(mainView,
-                (float)(width * widthScaleLeft),
-                (float)(height * (heightScaleTop + heightScaleBot) + scaleButton),
-                scaleWidthMain,
-                (float)(height * 0.55));
+                (float)(width * widthScale),
+                (float)(height * heightScalePlayers + heightBtn),
+                widthMain,
+                heightMain);
         sb.draw(secondView,
-                (float)(scaleWidthMain + 0.05*width),
-                (float)(height * (heightScaleTop + heightScaleBot + 2*heightScalePlayers+ 2*0.01) + scaleButton),
-                scaleWidthPlayers,
-                (float)(height * heightScalePlayers));
+                (float)(width * 3*widthScale + widthMain),
+                (float)(height * heightScalePlayers + 2.2*heightPlayers + heightBtn),
+                widthPlayers,
+                heightPlayers);
         sb.draw(thirdView,
-                (float)(scaleWidthMain + 0.05*width),
-                (float)(height * (heightScaleTop + heightScaleBot + heightScalePlayers + 0.01) + scaleButton),
-                scaleWidthPlayers,
-                (float)(height * heightScalePlayers));
+                (float)(width * 3*widthScale + widthMain),
+                (float)(height * heightScalePlayers + 1.1*heightPlayers + heightBtn),
+                widthPlayers,
+                heightPlayers);
         sb.draw(fourthView,
-                (float)(scaleWidthMain + 0.05*width),
-                (float)(height * (heightScaleTop + heightScaleBot) + scaleButton),
-                scaleWidthPlayers,
-                (float)(height * heightScalePlayers));
+                (float)(width * 3*widthScale + widthMain),
+                (float)(height * (heightScaleTop + heightScaleBot) + heightBtn),
+                widthPlayers,
+                heightPlayers);
+
+
         sb.end();
     }
 
