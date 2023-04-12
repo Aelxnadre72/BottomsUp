8a63c8d87671f3ad2c6a4f8a44e591331d01aaac
Martine Erdal Mansåker <martiman@stud.ntnu.no>
2023-04-12 14:20:25 +0200

    #5 Started creating result screen

--- core/src/screens/ResultScreen.java
+++ core/src/screens/ResultScreen.java
@@ -1,11 +1,36 @@
 package screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class ResultScreen extends Screen {
 
+    float width = Gdx.graphics.getWidth();
+    float height = Gdx.graphics.getHeight();
+
+    float scaleWidth = (float)(Gdx.graphics.getWidth() * 0.6);
+    float scaleHeight = (float)(scaleWidth * 0.3);
+    float scaleLogo = (float)(scaleWidth * 0.75);
+    private Texture background;
+    private Texture background2;
+    private Texture logo;
+    private Texture resultText;
+    private Texture first;
+    private Texture second;
+    private Texture third;
+    private Texture fourth;
+
     public ResultScreen(GameScreenManager gsm) {
+
         super(gsm);
+        background = new Texture("background.png");
+        background2 = new Texture("darkerBackground.png");
+        first = new Texture("1.png");
+        second = new Texture("2.png");
+        third = new Texture("3.png");
+        fourth = new Texture("4.png");
     }
 
     @Override
