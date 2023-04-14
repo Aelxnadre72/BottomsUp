22f52eb3ff0177b7267992a368a599558fc46222
alexle <alexle@stud.ntnu.no>
2023-04-14 15:52:42 +0200

    #1 finished touch and tower game logic

--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -6,26 +6,28 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.List;
+
 import components.BlockTower;
 
 public class GameScreen extends Screen {
     private Texture background;
 
+    private Texture btn0;
     private Texture btn1;
     private Texture btn2;
     private Texture btn3;
-    private Texture btn4;
+    private Texture invisibleBlock;
+    private Texture errorBlock;
     private Texture mainView;
     private Texture secondView;
     private Texture thirdView;
     private Texture fourthView;
 
+    private Rectangle boundsBtn0;
     private Rectangle boundsBtn1;
     private Rectangle boundsBtn2;
     private Rectangle boundsBtn3;
-    private Rectangle boundsBtn4;
-
-    private Vector2 pointer;
 
     private BlockTower blockTower;
 
@@ -37,48 +39,83 @@ public class GameScreen extends Screen {
 
     private long timeoutDuration = 3000;
 
+    private float width;
+    private float height;
+
+    // standard sizes for buttons
+    private float widthBtn;
+    private float heightBtn;
+
+    // Scales for width and height for the different buttons and screen
+    private double heightScaleTop;
+    private double heightScaleBot;
+    private double widthScale;
+
 
     public GameScreen(GameScreenManager gsm) {
         super(gsm);
         background = new Texture("background.png");
-        btn1 = new Texture("block0.png");
-        btn2 = new Texture("block1.png");
-        btn3 = new Texture("block2.png");
-        btn4 = new Texture("block3.png");
+        btn0 = new Texture("block0.png");
+        btn1 = new Texture("block1.png");
+        btn2 = new Texture("block2.png");
+        btn3 = new Texture("block3.png");
+        errorBlock = new Texture("errorBlock.png");
+        invisibleBlock = new Texture("invisibleBlock.png");
+        blockTower = new BlockTower(50);
 
         mainView = new Texture("button.png");
         secondView = new Texture("button.png");
         thirdView = new Texture("button.png");
         fourthView = new Texture("button.png");
+
+        // width and height of phone screen
+        width = Gdx.graphics.getWidth();
+        height = Gdx.graphics.getHeight();
+
+        // standard sizes for buttons
+        widthBtn = (float)(width * 0.47);
+        heightBtn = (float)(widthBtn * 0.5);
+
+        // Scales for width and height for the different buttons and screen
+        heightScaleTop = 0.141;
+        heightScaleBot = 0.01;
+        widthScale = 0.02;
     }
 
     @Override
     protected void handleInput() {
-        if (Gdx.input.justTouched()) {
-            pointer.x = Gdx.input.getX();
-            pointer.y = Gdx.input.getY();
+        if (Gdx.input.isTouched(0)) {
+            int pointerX = Gdx.input.getX(0);
+            int pointerY = Gdx.input.getY(0);
+            System.out.println("abcde");
+
 
             if (blockTower.getCopyOfCurrentList().isEmpty()) {
                 // stopp spillet
 
             } else {
+                System.out.println("fghij");
 
-                if (timeoutTime < elapsedTime) {
 
-                    if (boundsBtn1.contains(pointer.x, pointer.y)) {
-                        //spørre til databasen med btn1 som info
+                //if (timeoutTime < elapsedTime) {
+
+                    if (isBoundedByBtn(0, pointerX, pointerY)) {
+                        System.out.println("test1");
                         checkPopTimeoutBlock(0);
 
-                    } else if (boundsBtn2.contains(pointer.x, pointer.y)) {
+                    } else if (isBoundedByBtn(1, pointerX, pointerY)) {
                         checkPopTimeoutBlock(1);
+                        System.out.println("test2");
 
-                    } else if (boundsBtn3.contains(pointer.x, pointer.y)) {
+                    } else if (isBoundedByBtn(2, pointerX, pointerY)) {
                         checkPopTimeoutBlock(2);
+                        System.out.println("test3");
 
-                    } else if (boundsBtn4.contains(pointer.x, pointer.y)) {
+                    } else if (isBoundedByBtn(3, pointerX, pointerY)) {
                         checkPopTimeoutBlock(3);
+                        System.out.println("test4");
                     }
-                }
+                //}
             }
         }
     }
@@ -91,33 +128,21 @@ public class GameScreen extends Screen {
 @Override
     public void render(SpriteBatch sb) {
 
-        // vet ikke helt hvor den skal stå ennå men skal med et sted
-        float width = Gdx.graphics.getWidth();
-        float height = Gdx.graphics.getHeight();
-
         // standard sizes for player screens
         float widthMain = (float)(width * 0.6);
         float heightMain = (float)(height * 0.55);
         float widthPlayers = (float)(width * 0.32);
         float heightPlayers = (float) (height * 0.21);
 
-        // standard sizes for buttons
-        float widthBtn = (float)(width * 0.47);
-        float heightBtn = (float)(widthBtn * 0.65);
-
-        // Scales for width and height for the different buttons and screen
-        double heightScaleTop = 0.141;
-        double heightScaleBot = 0.01;
-        double widthScale = 0.02;
         double heightScalePlayers = heightScaleBot + heightScaleTop;
 
         sb.begin();
         sb.draw(background, 0, 0, width, height);
         //sb.draw(logo, (float)(width * 0.2), (float)(height * 0.6), scaleWidth, scaleLogo);
-        sb.draw(btn1, (float)(width * widthScale),              (float)(height * heightScaleTop), widthBtn, heightBtn);
-        sb.draw(btn2, (float)(width * 2*widthScale + widthBtn), (float)(height * heightScaleTop), widthBtn, heightBtn);
-        sb.draw(btn3, (float)(width * widthScale),              (float)(height * heightScaleBot), widthBtn, heightBtn);
-        sb.draw(btn4, (float)(width * 2*widthScale + widthBtn), (float)(height * heightScaleBot), widthBtn, heightBtn);
+        sb.draw(btn0, (float)(width * widthScale),              (float)(height * heightScaleTop), widthBtn, heightBtn);
+        sb.draw(btn1, (float)(width * 2*widthScale + widthBtn), (float)(height * heightScaleTop), widthBtn, heightBtn);
+        sb.draw(btn2, (float)(width * widthScale),              (float)(height * heightScaleBot), widthBtn, heightBtn);
+        sb.draw(btn3, (float)(width * 2*widthScale + widthBtn), (float)(height * heightScaleBot), widthBtn, heightBtn);
 
         sb.draw(mainView,
                 (float)(width * widthScale),
@@ -141,13 +166,20 @@ public class GameScreen extends Screen {
                 heightPlayers);
 
         // draw blocktower
-        sb.draw(btn1, (float)(widthMain/5), (float)(height/2.7),(float)(width * 0.25),
+        List<Integer> bt = blockTower.getCopyOfCurrentList();
+        if(blockTower.getCurrentHeight() <= 3) {
+            for (int j = 0; j < 4-blockTower.getCurrentHeight(); j++) {
+                bt.add(4); // when the number 4 is read, and invisible block will render instead
+            }
+        }
+
+        sb.draw(getBlockTowerImage(bt.get(0)), (float)(widthMain/5), (float)(height/2.7),(float)(width * 0.25),
                 (float)(width * 0.25));
-        sb.draw(btn2, (float)(widthMain/5), (float)(height/2.10), (float)(width * 0.25),
+        sb.draw(getBlockTowerImage(bt.get(1)), (float)(widthMain/5), (float)(height/2.7 + width * 0.25), (float)(width * 0.25),
                 (float)(width * 0.25));
-        sb.draw(btn3, (float)(widthMain/5), (float)(height/(1.70)), (float)(width * 0.25),
+        sb.draw(getBlockTowerImage(bt.get(2)), (float)(widthMain/5), (float)(height/2.7 + width * 0.5), (float)(width * 0.25),
                 (float)(width * 0.25)) ;
-        sb.draw(btn4, (float)(widthMain/5), (float)(height/1.45), (float)(width * 0.25),
+        sb.draw(getBlockTowerImage(bt.get(3)), (float)(widthMain/5), (float)(height/2.7 + width * 0.75), (float)(width * 0.25),
                 (float)(width * 0.25));
 
         sb.end();
@@ -155,14 +187,76 @@ public class GameScreen extends Screen {
 
     @Override
     public void dispose() {
-
+        btn0.dispose();
+        btn1.dispose();
+        btn2.dispose();
+        btn3.dispose();
+        errorBlock.dispose();
+        mainView.dispose();
+        secondView.dispose();
+        thirdView.dispose();
+        fourthView.dispose();
+        invisibleBlock.dispose();
     }
 
     private void checkPopTimeoutBlock(int blockNumber) {
         if (blockTower.checkNextBlock(blockNumber)) { // sjekke om de matcher med ønsket knapp
             blockTower.popNextBlock();
+            //sender nyeste liste til databasen!!!!!
         } else {
             timeoutTime = System.currentTimeMillis() + timeoutDuration;
+        }
+    }
+
+    private Texture getBlockTowerImage(int blockNumber) {
+        if(blockNumber == 0) {
+            return btn0;
+        }
+        else if (blockNumber == 1){
+            return btn1;
+        }
+        else if (blockNumber == 2){
+            return btn2;
+        }
+        else if (blockNumber == 3){
+            return btn3;
+        }
+        // uses invisible block image when there is less than 4 blocks left of the tower
+        else if (blockNumber == 4) {
+            return invisibleBlock;
+        }
+        else {
+            return errorBlock;
+        }
+    }
+
+    private Boolean isBoundedByBtn(int btnNumber, int x, int y) {
+        //NB: sb.draw og gdx.input.getY/getX har forskjellig default grid.
+        // Origo til sb.draw er nederst til venstre, mens origo til gdx.input er øverst til venstre.
+
+        if(btnNumber == 0) {
+            return (x >= width*widthScale &&
+                    x <= width*widthScale+widthBtn &&
+                    y >= height-height*heightScaleTop-heightBtn &&
+                    y <= height-height*heightScaleTop);
+        }
+        else if (btnNumber == 1) {
+            return (x >= width*2*widthScale+widthBtn &&
+                    x <= width*widthScale+widthBtn*2 &&
+                    y >= height-height*heightScaleTop-heightBtn &&
+                    y <= height-height*heightScaleTop);
+        }
+        else if (btnNumber == 2) {
+            return (x >= width*widthScale &&
+                    x <= width*widthScale+widthBtn &&
+                    y >= height-height*heightScaleBot-heightBtn &&
+                    y <= height-height*heightScaleBot);
+        }
+        else {
+            return (x >= width*2*widthScale+widthBtn &&
+                    x <= width*widthScale+widthBtn*2 &&
+                    y >= height-height*heightScaleBot-heightBtn &&
+                    y <= height-height*heightScaleBot);
         }
     }
 }
