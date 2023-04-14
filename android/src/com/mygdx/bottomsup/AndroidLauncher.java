a3846392ec597572f34ccb663afd70867b1394bf
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2023-04-14 13:11:09 +0200

    #16 Test database structure

--- android/src/com/mygdx/bottomsup/AndroidLauncher.java
+++ android/src/com/mygdx/bottomsup/AndroidLauncher.java
@@ -14,7 +14,20 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new BottomsUp(), config);
 		FirebaseDatabase database = FirebaseDatabase.getInstance();
-		DatabaseReference myRef = database.getReference("message");
-		myRef.setValue("Hello, World!");
+		DatabaseReference players = database.getReference("players");
+		players.setValue("");
+		players.child("1");
+		players.child("1").child("name").setValue("Einar");
+		players.child("1").child("blockTower").setValue("1,2,1,4,1");
+		players.child("1").child("time").setValue("0");
+
+		players.child("2");
+		players.child("2").child("name").setValue("Jan Adrian");
+		players.child("2").child("blockTower").setValue("1,2,1,3,4");
+		players.child("2").child("time").setValue("0");
+
+		DatabaseReference highscores = database.getReference("highscores");
+		highscores.setValue("");
+		highscores.child("16473").child("name").setValue("Einar");
 	}
 }
