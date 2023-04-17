09a798d856931161472281cc58eae563f0d993b9
Martine Erdal Mansåker <martiman@stud.ntnu.no>
2023-04-17 14:47:21 +0200

    #1 Add avatar picture and nametags. Some design changes

--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -1,10 +1,12 @@
 package screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
 
 import java.util.List;
 
@@ -12,7 +14,9 @@ import components.BlockTower;
 
 public class GameScreen extends Screen {
     private Texture background;
-
+    private Texture background2;
+    private Texture backgroundWhite;
+    private Texture backgroundWhite2;
     private Texture btn0;
     private Texture btn1;
     private Texture btn2;
@@ -23,15 +27,19 @@ public class GameScreen extends Screen {
     private Texture secondView;
     private Texture thirdView;
     private Texture fourthView;
-
     private Texture boundsBtn0;
     private Texture boundsBtn1;
     private Texture boundsBtn2;
     private Texture boundsBtn3;
-
-    private BlockTower blockTower;
-
     private Texture timeoutSplash;
+    private Texture cancelButton;
+    private Texture avatar;
+    private Texture nametag1;
+    private Texture nametag2;
+    private Texture nametag3;
+    private Texture nametag4;
+    private Texture screenDivider;
+    private BlockTower blockTower;
 
     private long timeoutTime = 0;
 
@@ -49,6 +57,19 @@ public class GameScreen extends Screen {
     private float widthBtn;
     private float heightBtn;
 
+    // size for the avatar
+    private float widthAvatar;
+    private float heightAvatar;
+
+    private float widthNameTag1;
+    private float heightNametag1;
+    private float widthNameTag2;
+    private float heightNametag2;
+    private float widthNameTag3;
+    private float heightNametag3;
+    private float widthNameTag4;
+    private float heightNametag4;
+
     // Scales for width and height for the different buttons and screen
     private double heightScaleTop;
     private double heightScaleBot;
@@ -62,7 +83,6 @@ public class GameScreen extends Screen {
     private float widthMainBlock;
 
     private double heightScalePlayers;
-    private Texture cancelButton;
 
     private Rectangle boundsExitField;
 
@@ -71,9 +91,18 @@ public class GameScreen extends Screen {
 
     public GameScreen(GameScreenManager gsm) {
         super(gsm);
+        ShapeRenderer sr = new ShapeRenderer();
         blockTower = new BlockTower(50);
 
         background = new Texture("background.png");
+        background2 = new Texture("background2.png");
+        nametag1 = new Texture("background2.png");
+        nametag2 = new Texture("background2.png");
+        nametag3 = new Texture("background2.png");
+        nametag4 = new Texture("background2.png");
+        screenDivider = new Texture("background2.png");
+
+
         btn0 = new Texture("block0.png");
         btn1 = new Texture("block1.png");
         btn2 = new Texture("block2.png");
@@ -93,6 +122,8 @@ public class GameScreen extends Screen {
         thirdView = new Texture("button.png");
         fourthView = new Texture("button.png");
 
+        avatar = new Texture("avatar.png");
+
         // width and height of phone screen
         width = Gdx.graphics.getWidth();
         height = Gdx.graphics.getHeight();
@@ -100,6 +131,10 @@ public class GameScreen extends Screen {
         // standard sizes for buttons
         widthBtn = (float)(width * 0.47);
         heightBtn = (float)(widthBtn * 0.5);
+
+        // size for the avatar
+        widthAvatar = (float)(width * 0.47);
+        heightAvatar = (float)(widthAvatar * 1.4);
 
         // Scales for width and height for the different buttons and screen
         heightScaleTop = 0.141;
@@ -109,8 +144,8 @@ public class GameScreen extends Screen {
         // standard sizes for player screens
         widthMain = (float)(width * 0.6);
         heightMain = (float) (height * 0.62);
-        widthPlayers = (float)(width * 0.32);
-        heightPlayers = (float) (height * 0.21);
+        widthPlayers = (float)(width * 0.38);
+        heightPlayers = (float) (height-(heightMain*0.105*3)-(height * 0.27))/3;
         widthMainBlock = (float)(width * 0.25);
 
         heightScalePlayers = heightScaleBot + heightScaleTop;
@@ -164,6 +199,9 @@ public class GameScreen extends Screen {
 
         sb.begin();
         sb.draw(background, 0, 0, width, height);
+        sb.draw(background2, 0, 0, width, (float)(height * 0.27));
+        sb.draw(screenDivider, widthMain, 0, (width-(widthPlayers)-widthMain), height);
+
         //sb.draw(logo, (float)(width * 0.2), (float)(height * 0.6), scaleWidth, scaleLogo);
         sb.draw(boundsBtn0, (float)(width * widthScale),              (float)(height * heightScaleTop), widthBtn, heightBtn);
         sb.draw(boundsBtn1, (float)(width * 2*widthScale + widthBtn), (float)(height * heightScaleTop), widthBtn, heightBtn);
@@ -171,28 +209,6 @@ public class GameScreen extends Screen {
         sb.draw(boundsBtn3, (float)(width * 2*widthScale + widthBtn), (float)(height * heightScaleBot), widthBtn, heightBtn);
 
         sb.draw(cancelButton, (float)(width * 0.05), (float)(height * 0.92), scaleExit, scaleExit);
-
-        sb.draw(mainView,
-                (float)(width * widthScale),
-                (float)(height * heightScalePlayers + heightBtn),
-                widthMain,
-                heightMain);
-        sb.draw(secondView,
-                (float)(width * 3*widthScale + widthMain),
-                (float)(height * heightScalePlayers + 2.2*heightPlayers + heightBtn),
-                widthPlayers,
-                heightPlayers);
-        sb.draw(thirdView,
-                (float)(width * 3*widthScale + widthMain),
-                (float)(height * heightScalePlayers + 1.1*heightPlayers + heightBtn),
-                widthPlayers,
-                heightPlayers);
-        sb.draw(fourthView,
-                (float)(width * 3*widthScale + widthMain),
-                (float)(height * (heightScaleTop + heightScaleBot) + heightBtn),
-                widthPlayers,
-                heightPlayers);
-
         // draw blocktower
         List<Integer> bt = blockTower.getCopyOfCurrentList();
         // replaces the last block images with an invisible block when there is less than 4 blocks left
@@ -203,25 +219,45 @@ public class GameScreen extends Screen {
         }
 
         if(timeoutTime < System.currentTimeMillis()) {
-            sb.draw(getBlockTowerImage(bt.get(0)), (float) (widthMain / 2 - widthMainBlock * 0.42), (float) (height / 2.7), widthMainBlock,
+            sb.draw(getBlockTowerImage(bt.get(0)), (float) (widthMain / 2.85 + widthMainBlock * 0.42), (float) (height / 2.55), widthMainBlock,
                     widthMainBlock);
-            sb.draw(getBlockTowerImage(bt.get(1)), (float) (widthMain / 2 - widthMainBlock * 0.42), (float) (height / 2.7 + width * 0.25), widthMainBlock,
+            sb.draw(getBlockTowerImage(bt.get(1)), (float) (widthMain / 2.85 + widthMainBlock * 0.42), (float) (height / 2.55 + width * 0.25), widthMainBlock,
                     widthMainBlock);
-            sb.draw(getBlockTowerImage(bt.get(2)), (float) (widthMain / 2 - widthMainBlock * 0.42), (float) (height / 2.7 + width * 0.5), widthMainBlock,
+            sb.draw(getBlockTowerImage(bt.get(2)), (float) (widthMain / 2.85 + widthMainBlock * 0.42), (float) (height / 2.55 + width * 0.5), widthMainBlock,
                     widthMainBlock);
-            sb.draw(getBlockTowerImage(bt.get(3)), (float) (widthMain / 2 - widthMainBlock * 0.42), (float) (height / 2.7 + width * 0.75), widthMainBlock,
+            sb.draw(getBlockTowerImage(bt.get(3)), (float) (widthMain / 2.85 + widthMainBlock * 0.42), (float) (height / 2.55 + width * 0.75), widthMainBlock,
                     widthMainBlock);
+            // avatar and nametags drawn in both if and else since it needs to be behind the splat,
+            // but in front of the blocks
+            sb.draw(avatar, (widthMain / 3 - widthMainBlock), (float)(height / 3.2),
+                    widthAvatar, heightAvatar);
+            sb.draw(nametag1, 0, (float)(height * 0.84), widthMain, (float)(heightMain*0.105));
+            sb.draw(nametag2, (width-(widthPlayers)), height-(float)(heightMain*0.105), widthPlayers,
+                    (float)(heightMain*0.105));
+            sb.draw(nametag3, (width-(widthPlayers)), (float)(height-heightMain*0.105*2-heightPlayers), widthPlayers,
+                    (float)(heightMain*0.105));
+            sb.draw(nametag4, (width-(widthPlayers)), (float)(height-(heightMain*0.105*3)-heightPlayers*2), widthPlayers,
+                    (float)(heightMain*0.105));
         }
         else {
+            sb.draw(avatar, (widthMain / 3 - widthMainBlock), (float)(height / 3.2),
+                    widthAvatar, heightAvatar);
+            sb.draw(nametag1, 0, (float)(height * 0.84), widthMain, (float)(heightMain*0.105));
+            sb.draw(nametag2, (width-(widthPlayers)), height-(float)(heightMain*0.105), widthPlayers,
+                    (float)(heightMain*0.105));
+            sb.draw(nametag3, (width-(widthPlayers)), (float)(height-heightMain*0.105*2-heightPlayers), widthPlayers,
+                    (float)(heightMain*0.105));
+            sb.draw(nametag4, (width-(widthPlayers)), (float)(height-(heightMain*0.105*3)-heightPlayers*2), widthPlayers,
+                    (float)(heightMain*0.105));
             sb.draw(timeoutSplash,0,0, width, height);
             // makes the tower invisible while there is a timeout/splash
-            sb.draw(invisibleBlock, (float) (widthMain / 2 - widthMainBlock * 0.42), (float) (height / 2.7), widthMainBlock,
+            sb.draw(invisibleBlock, (float) (widthMain / 2.85 - widthMainBlock * 0.42), (float) (height / 2.47), widthMainBlock,
                     widthMainBlock);
-            sb.draw(invisibleBlock, (float) (widthMain / 2 - widthMainBlock * 0.42), (float) (height / 2.7 + width * 0.25), widthMainBlock,
+            sb.draw(invisibleBlock, (float) (widthMain / 2.85 - widthMainBlock * 0.42), (float) (height / 2.47 + width * 0.25), widthMainBlock,
                     widthMainBlock);
-            sb.draw(invisibleBlock, (float) (widthMain / 2 - widthMainBlock * 0.42), (float) (height / 2.7 + width * 0.5), widthMainBlock,
+            sb.draw(invisibleBlock, (float) (widthMain / 2.85 - widthMainBlock * 0.42), (float) (height / 2.47 + width * 0.5), widthMainBlock,
                     widthMainBlock);
-            sb.draw(invisibleBlock, (float) (widthMain / 2 - widthMainBlock * 0.42), (float) (height / 2.7 + width * 0.75), widthMainBlock,
+            sb.draw(invisibleBlock, (float) (widthMain / 2.85 - widthMainBlock * 0.42), (float) (height / 2.47 + width * 0.75), widthMainBlock,
                     widthMainBlock);
         }
 
